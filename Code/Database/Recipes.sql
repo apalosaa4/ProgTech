f2737da942a8fcbc1d85bfa7b8a5de07b3feaf6f
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-01 20:34:32 +0200

    Recipes database changed

--- Code/Database/Recipes.sql
+++ Code/Database/Recipes.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `Ingredients` (
   `ingredients_id` int PRIMARY KEY AUTO_INCREMENT,
-  `ingredients_name` varchar(255),
-  `ingredients_freefrom_name` varchar(255)
+  `ingredients_name` varchar(255)
 );
 
 CREATE TABLE `FreeFrom` (
@@ -17,6 +16,23 @@ CREATE TABLE `Recipes` (
   `recipes_description` text
 );
 
-ALTER TABLE `Ingredients` ADD FOREIGN KEY (`ingredients_freefrom_name`) REFERENCES `FreeFrom` (`freefrom_id`);
+CREATE TABLE `Recipe_Ingredients` (
+  `recipe_id` int,
+  `ingredients_id` int,
+  `ingredients_amount` varchar(255)
+);
+
+CREATE TABLE `Recipe_Freefrom` (
+  `freefrom_id` int,
+  `recipe_id` int
+);
+
+ALTER TABLE `Recipe_Freefrom` ADD FOREIGN KEY (`recipe_id`) REFERENCES `Recipes` (`recipes_id`);
+
+ALTER TABLE `Recipe_Freefrom` ADD FOREIGN KEY (`freefrom_id`) REFERENCES `FreeFrom` (`freefrom_id`);
+
+ALTER TABLE `Recipe_Ingredients` ADD FOREIGN KEY (`ingredients_id`) REFERENCES `Ingredients` (`ingredients_id`);
+
+ALTER TABLE `Recipe_Ingredients` ADD FOREIGN KEY (`recipe_id`) REFERENCES `Recipes` (`recipes_id`);
 
 ALTER TABLE `Recipes` ADD FOREIGN KEY (`ingredients_name`) REFERENCES `Ingredients` (`ingredients_id`);
