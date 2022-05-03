6b1efcd9956ef21f6f3452b00165d4c6a5a7a0cd
KulcsarVivi <91262055+KulcsarVivi@users.noreply.github.com>
2022-05-03 22:38:52 +0200

    New recipe design added

--- Code/JAVA/Recipes/src/recipes_gui/NewRecipe.java
+++ Code/JAVA/Recipes/src/recipes_gui/NewRecipe.java
@@ -3,4 +3,14 @@ package recipes_gui;
 import javax.swing.*;
 
 public class NewRecipe {
+    private JLabel AddNewRecipeLbl;
+    private JLabel RecipeNameLbl;
+    private JTextField RecipeNameTf;
+    private JLabel FreeLbl;
+    private JCheckBox GlutenFreeCb;
+    private JCheckBox MilkFreeCb;
+    private JCheckBox VeganCb;
+    private JLabel GlutenFreeLbl;
+    private JLabel VeganLbl;
+    private JLabel MilfFreeLbl;
 }
