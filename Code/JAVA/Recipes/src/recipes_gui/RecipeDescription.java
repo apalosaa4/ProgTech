126b01c7e41259af1d459c995c6f359f86c7a60f
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-04 21:11:40 +0200

    RecipeDescription form base added

--- Code/JAVA/Recipes/src/recipes_gui/RecipeDescription.java
+++ Code/JAVA/Recipes/src/recipes_gui/RecipeDescription.java
@@ -1,4 +1,10 @@
 package recipes_gui;
 
+import javax.swing.*;
+
 public class RecipeDescription {
+    private JTextField Recipe_name;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
 }
