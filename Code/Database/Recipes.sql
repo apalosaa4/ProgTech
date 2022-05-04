53c22193e553a2b3cd71a412e8028fafb605f9f6
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-04 21:50:01 +0200

    Database issues corrected

--- Code/Database/Recipes.sql
+++ Code/Database/Recipes.sql
@@ -1,38 +1,162 @@
-CREATE TABLE `Ingredients` (
-  `ingredients_id` int PRIMARY KEY AUTO_INCREMENT,
-  `ingredients_name` varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Máj 04. 21:48
+-- Kiszolgáló verziója: 10.4.11-MariaDB
+-- PHP verzió: 7.4.3
 
-CREATE TABLE `FreeFrom` (
-  `freefrom_id` int PRIMARY KEY,
-  `freefrom_name` varchar(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `Recipes` (
-  `recipes_id` int PRIMARY KEY,
-  `recipes_name` varchar(255),
-  `freefrom_name` varchar(255),
-  `ingredients_name` varchar(255),
-  `recipes_description` text
-);
 
-CREATE TABLE `Recipe_Ingredients` (
-  `recipe_id` int,
-  `ingredients_id` int,
-  `ingredients_amount` varchar(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `Recipe_Freefrom` (
-  `freefrom_id` int,
-  `recipe_id` int
-);
+--
+-- Adatbázis: `recipes`
+--
 
-ALTER TABLE `Recipe_Freefrom` ADD FOREIGN KEY (`recipe_id`) REFERENCES `Recipes` (`recipes_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Recipe_Freefrom` ADD FOREIGN KEY (`freefrom_id`) REFERENCES `FreeFrom` (`freefrom_id`);
+--
+-- Tábla szerkezet ehhez a táblához `freefrom`
+--
 
-ALTER TABLE `Recipe_Ingredients` ADD FOREIGN KEY (`ingredients_id`) REFERENCES `Ingredients` (`ingredients_id`);
+CREATE TABLE `freefrom` (
+  `freefrom_id` int(11) NOT NULL,
+  `freefrom_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `Recipe_Ingredients` ADD FOREIGN KEY (`recipe_id`) REFERENCES `Recipes` (`recipes_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Recipes` ADD FOREIGN KEY (`ingredients_name`) REFERENCES `Ingredients` (`ingredients_id`);
+--
+-- Tábla szerkezet ehhez a táblához `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ingredients_id` char(255) NOT NULL,
+  `ingredients_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `recipes_id` int(11) NOT NULL,
+  `recipes_name` varchar(255) DEFAULT NULL,
+  `freefrom_name` varchar(255) DEFAULT NULL,
+  `ingredients_name` char(255) DEFAULT NULL,
+  `recipes_description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `recipe_freefrom`
+--
+
+CREATE TABLE `recipe_freefrom` (
+  `freefrom_id` int(11) DEFAULT NULL,
+  `recipe_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `recipe_ingredients`
+--
+
+CREATE TABLE `recipe_ingredients` (
+  `recipe_id` int(11) DEFAULT NULL,
+  `ingredients_id` char(255) DEFAULT NULL,
+  `ingredients_amount` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `freefrom`
+--
+ALTER TABLE `freefrom`
+  ADD PRIMARY KEY (`freefrom_id`);
+
+--
+-- A tábla indexei `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ingredients_id`);
+
+--
+-- A tábla indexei `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`recipes_id`),
+  ADD KEY `ingredients_name` (`ingredients_name`);
+
+--
+-- A tábla indexei `recipe_freefrom`
+--
+ALTER TABLE `recipe_freefrom`
+  ADD KEY `recipe_id` (`recipe_id`),
+  ADD KEY `freefrom_id` (`freefrom_id`);
+
+--
+-- A tábla indexei `recipe_ingredients`
+--
+ALTER TABLE `recipe_ingredients`
+  ADD KEY `ingredients_id` (`ingredients_id`),
+  ADD KEY `recipe_id` (`recipe_id`);
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `freefrom`
+--
+ALTER TABLE `freefrom`
+  ADD CONSTRAINT `Constrait_Recipe_Freefrom_FK` FOREIGN KEY (`freefrom_id`) REFERENCES `recipe_freefrom` (`freefrom_id`);
+
+--
+-- Megkötések a táblához `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD CONSTRAINT `Constrait_RECING_FK` FOREIGN KEY (`ingredients_id`) REFERENCES `recipe_ingredients` (`ingredients_id`),
+  ADD CONSTRAINT `Constrait_Recipes_FK` FOREIGN KEY (`ingredients_id`) REFERENCES `recipes` (`ingredients_name`);
+
+--
+-- Megkötések a táblához `recipes`
+--
+ALTER TABLE `recipes`
+  ADD CONSTRAINT `Constrait_RecipesFF_FK` FOREIGN KEY (`recipes_id`) REFERENCES `recipe_freefrom` (`recipe_id`),
+  ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`ingredients_name`) REFERENCES `ingredients` (`ingredients_id`);
+
+--
+-- Megkötések a táblához `recipe_freefrom`
+--
+ALTER TABLE `recipe_freefrom`
+  ADD CONSTRAINT `recipe_freefrom_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipes_id`),
+  ADD CONSTRAINT `recipe_freefrom_ibfk_2` FOREIGN KEY (`freefrom_id`) REFERENCES `freefrom` (`freefrom_id`);
+
+--
+-- Megkötések a táblához `recipe_ingredients`
+--
+ALTER TABLE `recipe_ingredients`
+  ADD CONSTRAINT `recipe_ingredients_ibfk_1` FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`ingredients_id`),
+  ADD CONSTRAINT `recipe_ingredients_ibfk_2` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipes_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
