dd5927c815f1f9dddd2f581c8015e458117b3e0c
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-09 22:27:46 +0200

    Logging added

--- Code/JAVA/Recipes/src/recipes_gui/DatabaseConnection.java
+++ Code/JAVA/Recipes/src/recipes_gui/DatabaseConnection.java
@@ -1,15 +1,20 @@
 package recipes_gui;
 
-//import org.apache.log4j.*;
+import org.apache.log4j.*;
 import javax.swing.*;
 import java.util.Arrays;
 import java.io.IOException;
 import java.sql.*;
 
 public class DatabaseConnection {
+    private static Logger logger = Logger.getLogger(DatabaseConnection.class);
+
     public static void main(String[] args) {
 
-        String host = System.getenv("DB_HOST");
+        logger.info("Proba");
+        System.out.println("Proba done");
+
+       /*String host = System.getenv("DB_HOST");
         String port = System.getenv("DB_PORT");
         String dbname = System.getenv("DB_NAME");
         String url = String.format("jdbc:mysql://localhost:3306/recipes",host,port,dbname);
@@ -17,6 +22,6 @@ public class DatabaseConnection {
         String password = System.getenv("DB_PASSWORD");
 
         JDBCUtil jdbcUtil = new JDBCUtil("com.mysql.cj.jdbc.Driver",url,username,password);
-        jdbcUtil.getConnection();
+        jdbcUtil.getConnection();*/
     }
 }
