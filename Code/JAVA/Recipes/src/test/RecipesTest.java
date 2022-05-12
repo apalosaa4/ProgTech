f3c3be8647cbeddc897aaf30eb8688637d831960
Viktoria Bognar <apalosaa4@gmail.com>
2022-05-12 23:50:06 +0200

    Update RecipesTest.java

--- Code/JAVA/Recipes/src/test/RecipesTest.java
+++ Code/JAVA/Recipes/src/test/RecipesTest.java
@@ -1,15 +1,61 @@
 package test;
 
+import Exceptions.RecipesNameCannotBeEmptyException;
+import Exceptions.RecipesNameTooShortException;
+import Exceptions.RecipesDescriptionCannotBeEmptyException;
+import Exceptions.RecipesDescriptionTooShortException;
 import Observer.RecipesClass;
 import org.junit.Test;
 import org.junit.Assert;
 import recipes_gui.Recipes;
 
-public class RecipesTest {
+public class RecipesTest
+{
 
     @Test
-    public void getRecipeName(){
+    public void getRecipeName()
+    {
         RecipesClass testRecipe = new RecipesClass("testRecipeName", "testFreeFromName", "testText");
         Assert.assertEquals("testRecipeName", testRecipe.getRecipeName());
+    }
+
+    @Test
+    public void getFreefromName()
+    {
+
+    }
+
+    @Test
+    public void getDescription()
+    {
+
+    }
+
+    @Test
+    public void RecipesNameCannotBeEmptyException()
+    {
+        Exception exception = Assert.assertThrows(RecipesNameCannotBeEmptyException.class, () ->{
+            RecipesClass testRecipes = new RecipesClass("", "testFreeFromName", "testText");
+        });
+    }
+
+    @Test
+    public void RecipesNameTooShortException()
+    {
+        Exception exception = Assert.assertThrows(RecipesNameTooShortException.class, () ->{
+            RecipesClass testRecipes = new RecipesClass("testRec", "testFreeFromName", "testText");
+        });
+    }
+
+    @Test
+    public void RecipesDescriptionCannotBeEmptyException()
+    {
+
+    }
+
+    @Test
+    public void RecipesDescriptionTooShortException()
+    {
+
     }
 }
