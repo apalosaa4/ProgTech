cc2217ecc8567a10b79b370f24429e003d168da5
KulcsarVivi <91262055+KulcsarVivi@users.noreply.github.com>
2022-05-18 21:58:00 +0200

    Create Measurement class

---
+++ Code/JAVA/Recipes/src/Classes/Measurement.java
@@ -0,0 +1,80 @@
+package Classes;
+
+import Exceptions.*;
+
+import javax.swing.*;
+
+import static java.util.Objects.isNull;
+
+public class Measurement {
+    private static int measurementId;
+    private String measurementName;
+    private double measurementAmount;
+
+    public static int getMeasurementId() {
+        return measurementId;
+    }
+
+    public static void setMeasurementId(int measurementId) {
+        if (measurementId == 0){
+            JOptionPane.showMessageDialog(null,
+                    "The ID cannot be zero.",
+                    "Warning!",
+                    JOptionPane.WARNING_MESSAGE);
+            //throw new MeasurementIDCannotBeEmptyException();
+        }
+        Measurement.measurementId = measurementId;
+    }
+
+    public String getMeasurementName() {
+        return measurementName;
+    }
+
+    public void setMeasurementName(String measurementName) {
+        if (measurementName.length() < 3)
+        {
+            JOptionPane.showMessageDialog(null,
+                    "The length of the measurement name must be at least 3 characters long!",
+                    "Warning!",
+                    JOptionPane.WARNING_MESSAGE);
+            //throw new MeasurementNameTooShortException();
+        }
+        if (isNull(measurementName)){
+            JOptionPane.showMessageDialog(null,
+                    "Measurement name cannot be null.",
+                    "Warning!",
+                    JOptionPane.WARNING_MESSAGE);
+            //throw new MeasurementNameCannotBeEmptyException();
+        }
+        this.measurementName = measurementName;
+    }
+
+    public double getMeasurementAmount() {
+        return measurementAmount;
+    }
+
+    public void setMeasurementAmount(double measurementAmount) {
+        if (measurementAmount <= 0)
+        {
+            JOptionPane.showMessageDialog(null,
+                    "The amount of measurement must not be less than zero.",
+                    "Warning!",
+                    JOptionPane.WARNING_MESSAGE);
+            //throw new MeasurementAmountTooShortException();
+        }
+        if (measurementAmount == 0){
+            JOptionPane.showMessageDialog(null,
+                    "The amount of measurement cannot be zero.",
+                    "Warning!",
+                    JOptionPane.WARNING_MESSAGE);
+            //throw new MeasurementAmountCannotBeEmpty();
+        }
+        this.measurementAmount = measurementAmount;
+    }
+
+    public Measurement(int measurementId, double measurementAmount, String measurementName){
+        setMeasurementId(measurementId);
+        setMeasurementName(measurementName);
+        setMeasurementAmount(measurementAmount);
+    }
+}
